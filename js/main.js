0eb7783835962993736a2f41c84f66c04991a75a
Ahmad Chata <ahmadchata@gmail.com>
2021-03-02 22:14:42 +0100

    Update css file to change image

--- js/main.js
+++ js/main.js
@@ -10,9 +10,12 @@ function darkmode() {
   const wasDarkmode = localStorage.getItem('darkmode') === 'true';
   localStorage.setItem('darkmode', !wasDarkmode);
   const element = document.body;
+  const animal = document.querySelector('animal');
   element.classList.toggle('dark-mode', !wasDarkmode);
+  // animal.textContent.toggle('dark-mode', !wasDarkmode);
 }
 
 function onload() {
   document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
+  document.querySelector('animal').toggle(textContent = '🦉');
 }
