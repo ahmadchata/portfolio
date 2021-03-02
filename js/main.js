2bba789b9e30898c1e0b48e46fe5ae3bc4b13744
Ahmad Chata <ahmadchata@gmail.com>
2021-03-02 21:45:50 +0100

    Add javascript for dark mode

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ $(document).ready(function() {
     $('h1').css('color','#222222');
   }, 1000);
 });
+
+//Dark mode
+function darkmode() {
+  const wasDarkmode = localStorage.getItem('darkmode') === 'true';
+  localStorage.setItem('darkmode', !wasDarkmode);
+  const element = document.body;
+  element.classList.toggle('dark-mode', !wasDarkmode);
+}
+
+function onload() {
+  document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
+}
