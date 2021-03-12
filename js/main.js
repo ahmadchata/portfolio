4642fc806a87153b04fb8084989667bb4f0d738e
Ahmad Chata <ahmadchata@gmail.com>
2021-03-12 10:17:56 +0100

    Update logo for dark mode

--- js/main.js
+++ js/main.js
@@ -7,14 +7,26 @@ $(document).ready(function() {
 
 //Dark mode
 function darkmode() {
-  const wasDarkmode = localStorage.getItem('darkmode') === 'true';
-  localStorage.setItem('darkmode', !wasDarkmode);
   const element = document.body
-  element.classList.toggle('dark-mode', !wasDarkmode);
+  element.classList.toggle('dark-mode');
 }
 
-function onload() {
-  element.classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
+function darkAnimal() {
+  var elem = document.getElementById("test");
+    if (elem.className === "animal") {
+      elem.className = "dark-animal";
+    } else {
+      elem.className = "animal";
+    }
+  }
+
+function enableDarkMode {
+  darkmode();
+  darkAnimal();
 }
+
+// function onload() {
+//   element.classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
+// }
  const btn = document.querySelector('#flexSwitchCheckChecked');
- btn.onclick = (e) => darkmode();
+ btn.addEventListener('click', enableDarkMode);
