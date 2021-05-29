39c1e7dd6ef4fb89fb5a4d5235814713bd1dc8d0
Ahmad Chata <ahmadchata@gmail.com>
2021-05-29 23:25:15 +0100

    Refactor code to arrow functions

--- js/main.js
+++ js/main.js
@@ -1,38 +1,38 @@
-$(document).ready(function() {
-  setTimeout(function(){
-    $('body').addClass('loaded');
-    $('h1').css('color','#222222');
+$(document).ready(function () {
+  setTimeout(function () {
+    $("body").addClass("loaded");
+    $("h1").css("color", "#222222");
   }, 3000);
 });
 
 //Dark mode
-function darkmode() {
-  const element = document.body
-  element.classList.toggle('dark-mode');
-}
+const darkMode = () => {
+  const element = document.body;
+  element.classList.toggle("dark-mode");
+};
 
-function darkAnimal() {
+const darkAnimal = () => {
   var elem = document.getElementById("animal");
-    if (elem.className === "animal") {
-      elem.className = "dark-animal";
-    } else {
-      elem.className = "animal";
-    }
-}
+  if (elem.className === "animal") {
+    elem.className = "dark-animal";
+  } else {
+    elem.className = "animal";
+  }
+};
 
-function darkArticle() {
+const darkArticle = () => {
   var elem = document.getElementById("article");
-    if (elem.className === "articles") {
-      elem.className = "dark-article";
-    } else {
-      elem.className = "articles";
-    }
-}
+  if (elem.className === "articles") {
+    elem.className = "dark-article";
+  } else {
+    elem.className = "articles";
+  }
+};
 
-function enableDarkMode() {
+const enableDarkMode = () => {
   darkAnimal();
-  darkmode();
+  darkMode();
   darkArticle();
-}
- const btn = document.querySelector('#flexSwitchCheckChecked');
- btn.addEventListener('click', enableDarkMode);
+};
+const btn = document.querySelector("#flexSwitchCheckChecked");
+btn.addEventListener("click", enableDarkMode);
